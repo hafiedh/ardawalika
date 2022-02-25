66e367ce8cfb973ffd74494e0f2ba8de3b5a355c
unknown <firdhair30@gmail.com>
2022-02-25 21:05:46 +0700

    menambahkan navbar dan footer

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const router = require("./routes/index");
 
 app.use(cors());
+app.use(express.static('views/assets'))
+app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
