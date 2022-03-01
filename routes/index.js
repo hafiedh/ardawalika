6e1a2c34406ea12d1ec9a4cdd7575926e36374d6
firdhair <firdhair30@gmail.com>
2022-03-01 15:13:25 +0000

    Create Dokumentasi Page

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,15 @@ const UserRouter = require("./UserRoute");
 
 router.use("/users", UserRouter);
 router.use(errorHandler);
+
 router.get("/", (req, res) => {
     res.render("index")
     console.log("index")
+})
+
+router.get("/dokumentasi", (req, res) => {
+    res.render("dokumentasi")
+    console.log("dokumentasi")
 })
 
 module.exports = router;
