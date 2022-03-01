0a9fbdb27c2933f4b147929ef5bbb55b1f196844
unknown <firdhair30@gmail.com>
2022-03-01 22:03:36 +0700

    menambahkan page dokumentasi

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,15 @@ const GoogleRouter = require("./GoogleRoute");
 router.use("/users", UserRouter);
 router.use("/auth/google", GoogleRouter);
 router.use(errorHandler);
+
 router.get("/", (req, res) => {
     res.render("index")
     console.log("index")
+})
+
+router.get("/dokumentasi", (req, res) => {
+    res.render("dokumentasi")
+    console.log("dokumentasi")
 })
 
 module.exports = router;
