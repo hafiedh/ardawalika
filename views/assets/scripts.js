eb96d89c262d1c3416caac4efdebcb1bdeb92239
unknown <firdhair30@gmail.com>
2022-03-06 16:25:50 +0700

    menambahkan carousel pada page order dan memisahkan script js dr footer ke file baru

--- views/assets/scripts.js
+++ views/assets/scripts.js
@@ -1,59 +1,3 @@
-<footer>
-    <div class="footer-container">
-        <div class="brand">
-            <h3 class="footer-brand">Ardawalika</h3>
-        </div>
-        <div class="layanan-kami">
-            <ul>
-                <li class="footer-title"><a href="#">Layanan Kami</a></li>
-                <li><a href="#">Wedding</a></li>
-                <li><a href="#">Family Gathering</a></li>
-                <li><a href="#">Corporate Gathering</a></li>
-            </ul>
-        </div>
-        <div class="tentang-kami">
-            <ul>
-                <li class="footer-title"><a href="#">Tentang Kami</a></li>
-                <li><a href="#">Vendor</a></li>
-                <li><a href="#">Galeri</a></li>
-                <li><a href="#">FAQ</a></li>
-            </ul>
-        </div>
-        <div class="alamat">
-            <ul>
-                <li class="footer-title"><a href="#">Alamat</a></li>
-                <li><a href="#">Jl. Merdeka, Pasar Minggu <br>Jakarta Timur <br>+62 88123123123</a></li>
-                <li class="footer-title temukan"><a href="#">Temukan Kami Di</a></li>
-            </ul>
-            <ul class="footer-logo">
-                <li>
-                    <a href="#"><img src="/img/instagram.svg" alt=""></a>
-                </li>
-                <li>
-                    <a href="#"><img src="/img/facebook.svg" alt=""></a>
-                </li>
-                <li>
-                    <a href="#"><img src="/img/whatsapp.svg" alt=""></a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <p>Copyright C 2022 by Ardawalika-</p>
-</footer>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-    crossorigin="anonymous"></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
-
-<script>
-
         // SCRIPT FOR THE LANDING PAGE'S NAVBAR //
         $(document).ready(function () {
             $(window).scroll(function () {
@@ -143,7 +87,3 @@
                 console.log("none")
             }
         }
-
-</script>
-</body>
-</html>
