8415779e1211f91ab5c8174c5d723ed2fe0a5f16
unknown <firdhair30@gmail.com>
2022-03-07 23:28:44 +0700

    mengubah footer dan membuat source code pad membuat source code pada halaman order lebih dry

--- views/assets/scripts.js
+++ views/assets/scripts.js
@@ -1,36 +1,3 @@
-        // QUERY SELECTOR //
-        const detailPaket = document.querySelector(".detail-paket")
-        const kustomisasi = document.querySelector(".kustomisasi")
-        const boxOptions = document.querySelector(".box-options")
-        const boxDetailPaket = document.querySelector(".box-detail-paket")
-        const boxKustomisasi = document.querySelector(".box-kustomisasi")
-        
-        const dekorasi = document.querySelector(".dekorasi");
-        const rias = document.querySelector(".rias")
-        const katering = document.querySelector(".katering")
-        const dokumentasi = document.querySelector(".documentation")
-        const hiburan = document.querySelector(".hiburan")
-        const mc = document.querySelector(".mc")
-
-        const dekorasiDetail = document.querySelector(".paket-detail-dekorasi");
-        const riasDetail = document.querySelector(".paket-detail-rias")
-        const kateringDetail = document.querySelector(".paket-detail-katering")
-        const dokumentasiDetail = document.querySelector(".paket-detail-documentation")
-        const hiburanDetail = document.querySelector(".paket-detail-hiburan")
-        const mcDetail = document.querySelector(".paket-detail-mc")
-        const showAll = document.querySelector(".showDoc")
-
-        const below = document.querySelector(".below")
-        const selengkapnya = document.querySelector("#selengkapnya")
-        const tanggal = document.querySelector(".tanggal")
-        
-        showAll.addEventListener("click", showDocumentation)
-        detailPaket.addEventListener("click", createActiveLeft)
-        kustomisasi.addEventListener("click", createActiveRight)
-
-        boxKustomisasi.style.display = "none"
-                
-
         // SCRIPT FOR THE LANDING PAGE'S NAVBAR //
         $(document).ready(function () {
             $(window).scroll(function () {
@@ -43,29 +10,19 @@
                     $('#main-head-ul').removeAttr('id');
                 }
             });
-
-            
-            // createActiveLeft()
-            // console.log("tessssssssss")
+            createActiveLeft()
+            console.log("tessssssssss")
         });
+
         
-        // SCRIPT TO SHOW ALL DOCUMENTATIONS //
-        function showDocumentation() {
-            console.log("aaaaaaaaa")
-
-            if(below.style.display == "none"){
-                below.style.display = "inline"
-                showAll.innerHTML = "Read More"
-                selengkapnya.style.display = "none"
-            } else {
-                 below.style.display = "none"
-                showAll.style.display = "none"
-                selengkapnya.style.display = "inline"
-            }
-        }
-
-
         // SCRIPT TO SHOW DETAIL PAKET //
+        const detailPaket = document.querySelector(".detail-paket")
+        const kustomisasi = document.querySelector(".kustomisasi")
+        const boxOptions = document.querySelector(".box-options")
+        
+        const boxDetailPaket = document.querySelector(".box-detail-paket")
+        const boxKustomisasi = document.querySelector(".box-kustomisasi")
+
         detailPaket.addEventListener("click", createActiveLeft)
         kustomisasi.addEventListener("click", createActiveRight)
 
@@ -78,7 +35,7 @@
                 boxDetailPaket.style.display = "block"
                 boxKustomisasi.style.display = "none"
                 console.log("block")
-                }
+            }
         }
 
         function createActiveRight() {
@@ -89,70 +46,29 @@
                 boxKustomisasi.style.display = "block"
                 boxDetailPaket.style.display = "none"
                 console.log("kustomisasi")
-                }
-        }
-
-        dekorasi.onclick = () => {
-            console.log("helooooo")
-            if (dekorasiDetail.style.display !== "block") {
-                dekorasiDetail.style.display = "block"
-            } else {
-                dekorasiDetail.style.display = "none"
-                console.log("none")
             }
         }
 
-        rias.onclick = () => {
-            console.log("yaa")
-            if (riasDetail.style.display !== "block") {
-                riasDetail.style.display = "block"
-                console.log("block")
+        const detailPaketChoices = document.querySelector(".detail-paket-choices")
+        detailPaketChoices.onclick = (e) => {
+            console.log("ini tes3")
+            const getClassName = e.target.className;
+            const getParent = e.target.parentNode.parentNode;
+            const getList = getParent.children[1];
+            console.log(getParent, getList)
+
+        if(getParent && getList && getClassName !== "detail-paket-choices" && getClassName !== "info-keterangan"){
+            if (getList.style.display !== "block") {
+                    getList.style.display = "block"
             } else {
-                riasDetail.style.display = "none"
-                console.log("none")
+                    getList.style.display = "none"
+                    console.log("none")
             }
+        } else {
+               return;
+           }
         }
 
-         katering.onclick = () => {
-            if (kateringDetail.style.display !== "block") {
-                kateringDetail.style.display = "block"
-                console.log("block")
-            } else {
-                kateringDetail.style.display = "none"
-                console.log("none")
-            }
-        }
 
-        dokumentasi.onclick = () => {
-            console.log("yaa")
-            if (dokumentasiDetail.style.display !== "block") {
-                dokumentasiDetail.style.display = "block"
-            } else {
-                dokumentasiDetail.style.display = "none"
-                console.log("none")
-            }
-        }
-
-        hiburan.onclick = () => {
-            console.log("yaa")
-            if (hiburanDetail.style.display !== "block") {
-                hiburanDetail.style.display = "block"
-            } else {
-                hiburanDetail.style.display = "none"
-                console.log("none")
-            }
-        }
-
-        mc.onclick = () => {
-            console.log("yaa")
-            if (mcDetail.style.display !== "block") {
-                mcDetail.style.display = "block"
-            } else {
-                mcDetail.style.display = "none"
-                console.log("none")
-            }
-        }
-
-        
 
    
