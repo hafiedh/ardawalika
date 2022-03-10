35dd7c170145209c01deb80009aeea9c9a0fd4b9
rizalmifta79 <rizalmifta79@gmail.com>
2022-03-10 22:58:32 +0700

    add crud halaman admin

--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -2,4 +2,13 @@ const AdminController = require("../controllers/AdminController");
 const router = require("express").Router();
 
 router.get('/dashboard',AdminController.viewDashboard)
+router.get('/dekorasi',AdminController.viewDecoration)
+router.post('/dekorasi',AdminController.createDecoration)
+router.put('/dekorasi',AdminController.updateDecoration)
+router.delete("/dekorasi/:id",AdminController.deleteDecoration)
+
+router.get('/catering',AdminController.viewCatering)
+router.post('/catering',AdminController.createCatering)
+router.put('/catering',AdminController.updateCatering)
+router.delete('/catering/:id',AdminController.deleteCatering)
 module.exports = router;
