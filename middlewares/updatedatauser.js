e62b7d38a60cc4641e25c1eec34f897cfb53a44f
hafiedh <muhhafiedh@gmail.com>
2022-03-12 20:04:39 +0700

    integrasi user profile

--- middlewares/updatedatauser.js
+++ middlewares/updatedatauser.js
@@ -1,15 +1,25 @@
 const { User } = require("../models");
 const { Op } = require("sequelize");
+const { decode } = require("../helpers/bcrypt");
 
 async function updateDataUser(req, res, next) {
   try {
     const { id, email } = req.user;
     const obj = JSON.parse(JSON.stringify(req.body));
-    const { username, fullname, phoneNumber, address } = obj;
+    const { username, fullname, phoneNumber, address, password, confirm_password, confirm_new_password } = obj;
     const currentUser = await User.findOne({
-      [Op.or]: [{ email }, { username }],
-      attributes: ["username", "fullname", "phoneNumber", "address"],
+      [Op.or]: [{ id }, { email }]
     });
+    if (!currentUser) {
+      throw { status: 400, message: "User not found" };
+    }
+    if (confirm_password !== confirm_new_password) {
+      throw { status: 400, message: "Confirm password and New password Not match" };
+    }
+    const passwordDecoded = decode(password, currentUser.password);
+    if (!passwordDecoded) {
+      throw { status: 400, message: "Old password is wrong" };
+    }
     const isDataChanged =
       JSON.stringify(currentUser) ===
       JSON.stringify({ username, fullname, phoneNumber, address });
@@ -25,6 +35,7 @@ async function updateDataUser(req, res, next) {
         fullname,
         phoneNumber,
         address,
+        password: password,
       },
       {
         where: { id, email },
