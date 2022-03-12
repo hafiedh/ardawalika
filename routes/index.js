3be6e59ac3a26c08124db9b427dccfa914538e36
hafiedh <muhhafiedh@gmail.com>
2022-03-12 13:08:02 +0000

    Feature/integrasiuserprofile

--- routes/index.js
+++ routes/index.js
@@ -93,12 +93,6 @@ router.get("/dokumentasi-detail", (req, res) => {
   console.log("dokumentasi-detail");
 });
 
-// PROFILE ROUTES //
-router.get("/profile", (req, res) => {
-  res.render("profile");
-  console.log("profile");
-});
-
 // tesDates //
 router.get("/konfirmasi", (req, res) => {
   res.render("konfirmasi");
