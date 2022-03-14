ec236ea5f62b86f4936be6180748f8bec62e9b6e
AJI SETIAJI <ajisetiaji101@gmail.com>
2022-03-14 21:57:18 +0700

    deploy database

--- config/config.js
+++ config/config.js
@@ -17,11 +17,11 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    username: "adkmicwwqkixap",
+    password: "2862834823e5787d5ee3c6d8dc0690c8fec89c5fcd09c742581b54def73167bf",
+    database: "d45g7m1rvp4b9n",
+    host: "ec2-3-231-254-204.compute-1.amazonaws.com",
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         rejectUnauthorized: false,
