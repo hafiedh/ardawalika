483bc64a28b46e44fae2af870c6a0f8c180203e0
hafiedh <muhhafiedh@gmail.com>
2022-04-09 21:25:49 +0700

    add filter multer

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,9 +1,22 @@
 const multer = require("multer");
 
 const storage = multer.memoryStorage();
+
+filter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 const upload = multer({
   storage: storage,
   limits: { fileSize: 8 * 1024 * 1024 },
+  fileFilter: fileFilter,
 });
 
 module.exports = upload;
